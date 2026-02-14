ce669e0befe0d75fe492f33d2bcb8d681bcd450c
tooosay <122712967+tooosay@users.noreply.github.com>
2026-02-14 12:15:13 +0000

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "libpcap-py";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          commonShellHook = ''
+            if command -v starship >/dev/null 2>&1; then
+              eval "$(starship init bash)"
+            fi
+          '';
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            package = with pkgs; [
+              python310
+              libpcap
+              uv
+            ];
+
+            shellHook = ''
+              ${commonShellHook}
+            '';
+          };
+        };
+      flake = {
+      };
+    };
+}
